8a85fd452aece39ea65990ec4e7b7d8d38893a4d
thanh binh <binhzone111@gmail.com>
2024-07-09 09:38:58 +0700

    stadium management

---
+++ frontend/src/components/priceList/PriceList.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { useRoute } from "vue-router";
+import { useStadium } from "../../stores/fetchStadium";
+import { onMounted, ref } from "vue";
+
+const stadiumData = ref([]);
+console.log("🚀 ~ stadiumData:", stadiumData);
+const stadiumStore = useStadium();
+const route = useRoute();
+onMounted(async () => {
+  await stadiumStore.getAStadium(route.params.id);
+  stadiumData.value = stadiumStore.stadiumData;
+});
+</script>
+<template>
+  <h1 class="text-2xl font-bold text-center">
+    Đặt sân {{ stadiumData.stadium_name }}
+  </h1>
+  <table id="price-list">
+    <tr>
+      <th>Sân</th>
+      <th>Giá</th>
+    </tr>
+
+    <tr>
+      <td>
+        <router-link
+          v-for="(stadiumStyle, index) in stadiumData.stadium_style"
+          :key="index"
+          :to="`/book-pitch/${stadiumStyle._id}`"
+          class="link"
+        >
+          <div class="flex items-center gap-2">
+            <span> {{ stadiumStyle.name }}</span>
+            <span> (Sân {{ stadiumStyle.type }})</span>
+          </div>
+        </router-link>
+      </td>
+      <td>
+        <router-link
+          v-for="(stadium_style, index) in stadiumData.stadium_style"
+          :key="index"
+          :to="`/book-pitch/${stadium_style._id}`"
+          class="link"
+        >
+          <div>{{ stadium_style.price }} Đ</div>
+        </router-link>
+      </td>
+    </tr>
+  </table>
+</template>
+<style>
+#price-list {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 25px 0;
+  text-align: left;
+}
+
+#price-list th,
+#price-list td {
+  padding: 12px 15px;
+  border: 1px solid #ddd;
+}
+
+#price-list th {
+  background-color: #04aa6d;
+  color: white;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+#price-list tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+#price-list td {
+  text-align: center;
+}
+
+.link {
+  display: block;
+  text-decoration: none;
+  color: inherit;
+}
+
+.link:hover {
+  background-color: #e0e0e0;
+}
+</style>
